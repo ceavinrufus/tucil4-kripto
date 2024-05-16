ccb3645626755f0f499096ac7a40214374c3d59b
Josua Adriel Sinabutar <18221065@std.stei.itb.ac.id>
2024-05-16 19:14:59 +0700

    creating framework

---
+++ Backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mysql = require('mysql')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+
+
+app.get('/', (re, res)=> {
+    return res.json("From Backend Side")
+})
+
+app.listen(8081, () => {
+    console.log("Server is running on port 8081")
+})
