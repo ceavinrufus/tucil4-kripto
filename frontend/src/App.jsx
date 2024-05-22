383aa446fa7c3b495e45ea284d4c4d0c1d3cb7c4
ceavinrufus <18221162@std.stei.itb.ac.id>
2024-05-22 18:09:39 +0700

    feat: add generate transcript

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Database from "./pages/Database";
+import Transcript from "./pages/Transcript";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/database" Component={Database} />
+        <Route path="/transcript" Component={Transcript} />
       </Routes>
     </div>
   );
