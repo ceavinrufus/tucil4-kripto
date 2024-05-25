78db4656ac6bf56f6986b9c6eed5e514655816a5
ceavinrufus <18221162@std.stei.itb.ac.id>
2024-05-25 23:54:33 +0700

    style: navbar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,31 @@
+import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import HeroImage from "../assets/teacher.jpg";
 
 const HomePage = () => {
+  const [isMobileView, setIsMobileView] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileView(window.innerWidth <= 767);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Initial check for mobile view on component mount
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="homepage">
       <Container>
-        <Row className="hero header-box d-flex align-items-center">
+        <Row
+          className={isMobileView ? "flex-column" : "d-flex align-items-center"}
+        >
           <Col>
             <h1>Selamat Datang!</h1>
             <h1>Tugas MK II403</h1>
@@ -22,10 +42,25 @@ const HomePage = () => {
               18221162 Ceavin Rufus De Prayer Purba
             </li>
           </Col>
-          <Col>
-            <img src={HeroImage} alt="hero-img" />
-          </Col>
+          {!isMobileView && (
+            <Col
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <img style={{ width: "80%" }} src={HeroImage} alt="hero-img" />
+            </Col>
+          )}
         </Row>
+        {isMobileView && (
+          <Row className="justify-content-center">
+            <Col xs={10} sm={8} md={6} lg={4}>
+              <img style={{ width: "100%" }} src={HeroImage} alt="hero-img" />
+            </Col>
+          </Row>
+        )}
       </Container>
     </div>
   );
